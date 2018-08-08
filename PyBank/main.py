f0bffa1bb78a4915259667eac3e31be9d000fed8
hongli719 <hongli719@gmail.com>
2018-08-08 11:20:12 -0700

    All done and the txt file is generated

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,6 +17,7 @@ with open(csvpath, newline='') as csvfile:
     total = 0.0
     compare_list = []
     str_list = []
+    change_list = []
     
 
     for row in csvreader:
@@ -28,21 +29,34 @@ with open(csvpath, newline='') as csvfile:
         compare_list.append(int(row[1]))
         str_list.append(row[0])
         
-
-
-            
+    for i in range(len(compare_list)-1):
         
-      
-      
+        # Make a list of every month changes
+        change = compare_list[i+1] - compare_list[i]
+        change_list.append(change)
 
+        
 
     print("\nFinancial Analysis")
     print("----------------------------")
     print(f"Total Months: {months}")
     print(f"Total: ${total}")
+    print(f"Average  Change: ${sum(change_list)/len(compare_list)}")
     print(f"Greatest Increase in Profits: {str_list[compare_list.index(max(compare_list))]} (${max(compare_list)})")
     print(f"Greatest Decrease in Profits: {str_list[compare_list.index(min(compare_list))]} (${min(compare_list)})")
     
+# Write it into a txt file
+file = open("Financial Analysis.txt", "w")
+
+file.write("Financial Analysis\n")
+file.write("----------------------------\n")
+file.write(f"Total Months: {months}\n")
+file.write(f"Total: ${total}\n")
+file.write(f"Average  Change: ${sum(change_list)/len(compare_list)}\n")
+file.write(f"Greatest Increase in Profits: {str_list[compare_list.index(max(compare_list))]} (${max(compare_list)})\n")
+file.write(f"Greatest Decrease in Profits: {str_list[compare_list.index(min(compare_list))]} (${min(compare_list)})\n")
+
+file.close()
     
 
     
